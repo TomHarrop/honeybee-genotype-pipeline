3659985ee9da390b97c026c827e867db8e8e00ff
Tom Harrop <twharrop@gmail.com>
2021-07-13 15:23:35 +1000

    ok but it was cached :S

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name='honeybee_genotype_pipeline',
-    version='0.0.14',
+    version='0.0.15',
     description='python3 wrapper for calling genotypes',
     long_description=readme(),
     url='https://github.com/tomharrop/honeybee-genotype-pipeline',
